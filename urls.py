38f0ef9b52ed7c6594633a0c05aec2ca06a9ad77
tougshire <tougshire@tougshire.com>
2022-05-01 21:03:17 -0400

    put back alt load without slack for compat..

--- urls.py
+++ urls.py
@@ -19,6 +19,7 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('loads/', include('ervinloads.urls')),
+    path('loads', include('ervinloads.urls')),
     path('accounts/', include('tougshire_auth.urls')),
 
 ]
