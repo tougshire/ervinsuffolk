748b1b42c51aa118aa20fbed55e7444f09645ed0
tougshire <tougshire@tougshire.com>
2022-05-08 13:23:32 -0400

    add home app

--- settings.py
+++ settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'anymail',
+    'home.apps.HomeConfig',
     'tougshire_history.apps.TougshireHistoryConfig',
     'touglates.apps.TouglatesConfig',
     'tougshire_vistas.apps.TougshireVistasConfig',
--- urls.py
+++ urls.py
@@ -18,6 +18,8 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('home.urls')),
+    path('home/', include('home.urls')),
     path('loads/', include('ervinloads.urls')),
     path('accounts/', include('tougshire_auth.urls')),
 
